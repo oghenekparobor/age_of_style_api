0554bc91d6daa1a9ae9c945b223cf47901562964
Eminokanju Oghenekparobor <66335632+oghenekparobor@users.noreply.github.com>
2022-01-05 01:13:23 +0100

    provider is being adjusted

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,8 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
-            \URL::forceScheme('https');
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
         }
+
+        Schema::defaultStringLength(191);
     }
 }
